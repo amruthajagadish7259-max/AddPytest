b0debf8a70c11288ec4c6d9bd8b61cc73fb41cf4
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-11-26 11:06:15 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -2,7 +2,13 @@ def add(a,b):
      return a+b
 
 if __name__ == "__main__":
+     x=int(input("Enter:")
+     y=int(input("Enter y:")
+     
+
+else:
      x=10
      y=20
-     print("Sum:",add(x,y))
+print("Sum:",add(x,y))
+
 
