4d604e72ba0fc56722b371abbf184eb1f5f6ea8a
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-11-26 11:28:28 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -2,13 +2,14 @@ def add(a,b):
      return a+b
 
 if __name__ == "__main__":
-    if len(sys.argv)>3
+    if len(sys.argv)>3:
        x=sys.argv[1]
       y=sys.argv[2]
 else:
      x=10
      y=20
 print("Sum:",add(x,y))
+
 
 
 
